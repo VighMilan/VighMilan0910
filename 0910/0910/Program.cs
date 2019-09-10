8413e19fa38283f698fd05f54f5e6d90aa84e87e
e18vighmilanzoltan <e18vighmilanzoltan@111-116>
2019-09-10 11:19:25 +0200

    Első commit

---
+++ 0910/0910/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _0910
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Readline();
+        }
+    }
+}
